98bc5b7b43aa90d1f717ba5909e510b816793d3e
yjh52110 <118456391+yjh52110@users.noreply.github.com>
2024-09-13 08:45:55 +0800

    Add files via upload

--- change.py
+++ change.py
@@ -2,6 +2,11 @@ import requests
 import json
 import time
 import threading
+import random
+
+def generate_random_password():
+    # 生成一个 6 位随机数
+    return str(random.randint(100000, 999999))
 
 def send_post_request():
     url = "https://fin.dding.net/v2/tenant/wechat/configure_password"
@@ -14,13 +19,16 @@ def send_post_request():
         "Referer": "https://servicewechat.com/wxb63528dc219b0805/26/page-frame.html",
     }
 
+    # 使用随机生成的 6 位数作为密码
+    password = generate_random_password()
+
     data = {
         "userid": "",
         "token": "ea9dc69c-30a5-4808-b208-49621a0717f8",
         "gcid": "",
         "wechatId": "",
         "params": {
-            "password": "923641",
+            "password": password,
             "compactId": "3EAC9E68A541DG4B95I8067G44B35F3021D7",
             "passwordId": "",
             "beginTime": "2024-09-01 00:00:00",
@@ -50,4 +58,4 @@ def start_threads():
 # 无限循环，每秒启动 10 个线程
 while True:
     start_threads()
-    # time.sleep(1)  # 每秒钟启动一次线程
+    time.sleep(1)  # 每秒钟启动一次线程
